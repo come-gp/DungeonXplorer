47fe1e5ab485e5db6af45323309bc280074b8fa7
Nathan Le Biez <nthnlbz@gmail.com>
2025-11-24 09:47:39 +0100

    commit pour changer de branche

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Dungeon Explorer</title>
@@ -13,7 +14,7 @@
         le heros d'aventure palpitant où vous explorez des donjons mystérieux, combattez des monstres redoutables et <br>
         découvrez des trésors cachés. Préparez-vous à vivre une expérience<br> inoubliable remplie d'action et de stratégie!</p>
     <div> 
-        <button >Nouvelle Partie</button>
+        <button class="btn .btn-gold">Nouvelle Partie</button>
     </div>
     <div>
         <button >Reprendre</button>
