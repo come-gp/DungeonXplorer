f947e92a125da8918abc5a6dcae4343a36d96ab6
Nathan Le Biez <nthnlbz@gmail.com>
2025-11-24 09:10:13 +0100

    petites modif accueil

--- index.php
+++ index.php
@@ -3,12 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Dungeon Explorer</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Dungeon Explorer</title>
 </head>
 <body>
+    <h1 class="mx-auto text-center">Dungeon Explorer</h1>
+    <p class="mx-auto text-center">Bienvenue dans Dungeon Explorer, un jeu dont vous êtes<br>
+        le heros d'aventure palpitant où vous explorez des donjons mystérieux, combattez des monstres redoutables et <br>
+        découvrez des trésors cachés. Préparez-vous à vivre une expérience<br> inoubliable remplie d'action et de stratégie!</p>
+    <div> 
+        <button >Nouvelle Partie</button>
+    </div>
+    <div>
+        <button >Reprendre</button>
+    </div>
+    <div>
+        <button >Charger</button>
+    </div>
     <h1 class="mx-auto text-center">Dungeon Explorer</h1>
     <p class="mx-auto text-center">Bienvenue dans Dungeon Explorer, un jeu dont vous êtes<br>
         le heros d'aventure palpitant où vous explorez des donjons mystérieux, combattez des monstres redoutables et <br>
