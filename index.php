ef0a55c547b6424618d819a4463afce9ee976027
Nathan Le Biez <nthnlbz@gmail.com>
2025-11-24 08:20:09 +0100

    ajout page d'accueil

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    <h1>Page d'acceuil du projet </h1>
-    <p>//TODO : faire le projet</p>
+    <h1>Dungeon Explorer</h1>
+    <button class="btn btn-danger">Nouvelle Partie</button>
+    <button class="btn btn-danger">Reprendre</button>
+    <button class="btn btn-danger">Charger</button>
 </body>
 </html>
