72ee0ffeae81a7e648ca5ed8227601df8d49376c
Nathan Le Biez <nthnlbz@gmail.com>
2025-12-26 18:26:33 +0100

    un peu de css page admin

--- app/Views/admin.php
+++ app/Views/admin.php
@@ -11,14 +11,18 @@
     <title>Dungeon Explorer</title>
 
     <link rel="stylesheet" href="<?= base_url('/css/navbar.css') ?>">
-    <link rel="stylesheet" href="<?= base_url('/css/home.css') ?>?v=<?= time() ?>">
+    <link rel="stylesheet" href="<?= base_url('/css/admin.css') ?>?v=<?= time() ?>">
 </head>
 <body>
-    <h2>Pannel Administrateur</h2>
+    <header>
+        <?php require __DIR__ . '/components/navbar.php'; ?>
+    </header>
+
+    <h2 class = "centered">Pannel Administrateur</h2>
 
     <form method="POST">
         <select name="choix" onchange="this.form.submit()">
-            <option value="">Choisir</option>
+            <option value="" disabled selected>Choisir</option>
             <option value="users">Utilisateurs</option>
             <option value="chapters">Chapitres</option>
             <option value="monsters">Monstres</option>
@@ -30,12 +34,12 @@
     <?php if (!empty($users) && $_POST['choix'] === 'users'){
         echo '<ul class="list-group">';
         foreach ($users as $user) {
-            echo '<li class="list-group-item align-items-center text-center">
-                    id: '.$user['id'].
-                    ', pseudo: '.$user['pseudo'].
+            echo '<li class="main-container">
+                    <b> Id </b>: '.$user['id'].
+                    ' <br> <b> Pseudo </b>: '.$user['pseudo'].
                     '<br>
                     <form method="POST" action="?controller=admin&action=delete">
-                        <button name="delete_user" value="'.$user['id'].'" class="btn btn-danger">Supprimer</button>
+                        <button name="delete_user" value="'.$user['id'].'" class="btn btn-primary">Supprimer</button>
                     </form>
                 </li>';
         }
@@ -43,13 +47,13 @@
     } elseif (!empty($chapters) && $_POST['choix'] === 'chapters'){
         echo '<ul class="list-group">';
         foreach ($chapters as $chapter) {
-            echo '<li class="list-group-item align-items-center text-center">
-            id: '.$chapter['id'].
-            ', titre: '.$chapter['title'].
-            ', contenu: '.$chapter['content'].
+            echo '<li class="main-container">
+            <b> Id </b>: '.$chapter['id'].
+            ' <br> <b> Titre </b>: '.$chapter['title'].
+            ' <br> <b> Contenu </b>: '.$chapter['content'].
             '<br>
                 <form method="POST" action="?controller=admin&action=delete">
-                    <button name="delete_chapter" value="'.$chapter['id'].'" class="btn btn-danger">Supprimer</button>
+                    <button name="delete_chapter" value="'.$chapter['id'].'" class="btn btn-primary">Supprimer</button>
                 </form>
             </li>';
         }
@@ -57,14 +61,14 @@
     } elseif (!empty($monsters) && $_POST['choix'] === 'monsters'){
         echo '<ul class="list-group">'; 
         foreach ($monsters as $monster) {
-            echo '<li class="list-group-item align-items-center text-center">
-                    id: '.$monster['id'].
-                    ', name: '.$monster['name'].
-                    ', pv: '.$monster['pv'].
-                    ', mana: '.$monster['mana'].
+            echo '<li class="main-container">
+                    <b> Id </b>: '.$monster['id'].
+                    ' <br> <b> Nom </b>: '.$monster['name'].
+                    ' <br> <b> PV </b>: '.$monster['pv'].
+                    ' <br> <b> Mana </b>: '.$monster['mana'].
                     '<br>
                     <form method="POST" action="">
-                        <button name="delete_monster" value="'.$monster['id'].'" class="btn btn-danger">Supprimer</button>
+                        <button name="delete_monster" value="'.$monster['id'].'" class="btn btn-primary">Supprimer</button>
                     </form>
                 </li>';
         }
@@ -72,14 +76,14 @@
     } elseif (!empty($items) && $_POST['choix'] === 'items'){
         echo '<ul class="list-group">'; 
         foreach ($items as $item) {
-            echo '<li class="list-group-item align-items-center text-center">
-                    id: '.$item['id'].
-                    ', name: '.$item['name'].
-                    ', description: '.$item['description'].
-                    ', type: '.$item['item_type'].
+            echo '<li class="main-container">
+                    <b> Id </b>: '.$item['id'].
+                    ' <br> <b> Nom </b>: '.$item['name'].
+                    ' <br> <b> Description </b>: '.$item['description'].
+                    ' <br> <b> Type </b>: '.$item['item_type'].
                     '<br>
                     <form method="POST" action="">
-                        <button name="delete_item" value="'.$item['id'].'" class="btn btn-danger">Supprimer</button>
+                        <button name="delete_item" value="'.$item['id'].'" class="btn btn-primary">Supprimer</button>
                     </form>
                 </li>';
         }
@@ -88,14 +92,14 @@
     elseif (!empty($classes) && $_POST['choix'] === 'class'){
         echo '<ul class="list-group">'; 
         foreach ($classes as $class) {
-            echo '<li class="list-group-item align-items-center text-center">
-                    id: '.$class['id'].
-                    ', name: '.$class['name'].
-                    ', description: '.$class['description'].
-                    ', base_pv: '.$class['base_pv'].
+            echo '<li class="main-container">
+                    <b> Id </b>: '.$class['id'].
+                    ' <br><b> Nom </b>: '.$class['name'].
+                    ' <br> <b> Description </b>: '.$class['description'].
+                    ' <br> <b> Base de pv </b>: '.$class['base_pv'].
                     '<br>
                     <form method="POST" action="">
-                        <button name="delete_class" value="'.$class['id'].'" class="btn btn-danger">Supprimer</button>
+                        <button name="delete_class" value="'.$class['id'].'" class="btn btn-primary">Supprimer</button>
                     </form>
                 </li>';
         }
